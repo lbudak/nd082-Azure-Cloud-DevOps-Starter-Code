e096a919bca0c2c7ffc13d38b1d99ac22ea698f2
lbudak <luka.budak@rt-rk.com>
2021-10-29 12:12:37 +0200

    terraform files

---
+++ C1 - Azure Infrastructure Operations/project/starter_files/terraform/main.tf
@@ -0,0 +1,156 @@
+provider "azurerm" {
+    features {}
+
+}
+
+resource "azurerm_resource_group" "main" { 
+    name = "${var.prefix}-resources"
+    location = var.location
+    tags = var.tagging
+}
+
+resource "azurerm_virtual_network" "main" {
+    name                = "${var.prefix}-network"
+    address_space       = ["10.0.0.0/16"]
+    location            = azurerm_resource_group.main.location
+    resource_group_name = azurerm_resource_group.main.name
+    tags = var.tagging
+}
+
+resource "azurerm_subnet" "internal" {
+    name                 = "internal"
+    resource_group_name  = azurerm_resource_group.main.name
+    virtual_network_name = azurerm_virtual_network.main.name
+    address_prefixes     = ["10.0.5.0/24"]
+    tags = var.tagging
+}
+
+resource "azurerm_public_ip" "pip" {
+    name  = "${var.prefix}-pip"
+    resource_group_name = azurerm_resource_group.main.name
+    location = azurerm_resource_group.main.location
+    allocation_method = "Static"
+    tags = var.tagging
+}
+
+resource "azurerm_network_interface" "main" {
+    count = var.num_of_instances
+
+    name = "${var.prefix}-nic${count.index}"
+    resource_group_name = azurerm_resource_group.main.name
+    location = azurerm_resource_group.main.location
+
+    ip_configuration {
+      name = "internal"
+      subnet_id = azurerm_subnet.internal.id
+      private_ip_address_allocation = "Dynamic"
+    }
+
+    tags = var.tagging
+}
+
+resource "azurerm_network_security_group" "main" {
+    name = "${var.prefix}-nsg"
+    location = azurerm_resource_group.main.location
+    resource_group_name = azurerm_resource_group.main.name
+
+    security_rule[
+      {
+        name                       = "InterVMAccess"
+        priority                   = 150
+        direction                  = "Outbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "*"
+        source_address_prefix      = azurerm_subnet.internal.address_prefix
+        destination_address_prefix = azurerm_subnet.internal.address_prefix
+      },
+      {
+        name                       = "DenyInternetAccess"
+        priority                   = 100
+        direction                  = "Inbound" 
+        access                     = "Deny"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "*"
+        source_address_prefix      = "Internet"
+        destination_address_prefix = azurerm_subnet.internal.address_prefix
+      }
+    ]
+
+  tags = var.tagging
+}
+
+resource "azurerm_lb" "main" {
+    name = "${var.prefix}-lb"
+    location = azurerm_resource_group.main.location
+    resource_group_name = azurerm_resource_group.main.name
+
+    frontend_ip_configuration {
+      name = "primary"
+      public_ip_address_id = azurerm_public_ip.pip.id
+    }
+
+    tags = var.tagging
+}
+
+resource "azurerm_lb_backend_address_pool" "main" {
+    resource_group_name = azurerm_resource_group.main.name
+    loadbalancer_id = azurerm_lb.main.id
+    name = "backendAddressPool"
+
+    tags = var.tagging
+}
+
+resource "azurerm_network_interface_backend_address_pool_association" "main" {
+    count = var.num_of_instances
+    
+    network_interface_id = element(azurerm_network_interface.main[*].id, count.index)
+    ip_configuration_name = "internal"
+    backend_address_pool_id = azurerm_lb_backend_address_pool.main.id
+
+    tags = var.tagging    
+}
+
+resource "azurerm_availability_set" "main" {
+    name = "${var.prefix}-avs"
+    location = azurerm_resource_group.main.location
+    resource_group_name = azurerm_resource_group.main.name
+    platform_update_domain_count = 3
+    platform_fault_domain_count = 3
+    managed = true
+
+    tags = var.tagging
+}
+
+data "azurerm_image" "packerImage" {
+  name = var.image_id.managed_image_name
+  resource_group_name = var.image_id.managed_image_resource_group_name
+}
+
+resource "azurerm_linux_virtual_machine" "main" {
+    count = var.num_of_instances 
+    
+    name = "${var.prefix}-${count.index}-vm"
+    resource_group_name = azurerm_resource_group.main.name
+    location = azurerm_resource_group.main.location
+    size = "Standard_D2s_v3"
+    admin_username = "${var.username}"
+    admin_password = "${var.password}"
+    disable_password_authentication = false
+    availability_set_id = azurerm_availability_set.main.id
+
+    network_interface_ids = [
+      azurerm_network_interface.main[count.index].id,
+    ]
+
+    source_image_id = data.azurerm_image.packerImage.id
+
+    os_disk {
+      storage_account_type = "Standard_LRS"
+      caching              = "ReadWrite"
+    }
+
+    tags = var.tagging
+}
---
+++ C1 - Azure Infrastructure Operations/project/starter_files/terraform/vars.tf
@@ -0,0 +1,52 @@
+variable "prefix" {
+    description = "The prefix which should be used for all resources"
+    type = string
+    default = "udacity-project-1"
+}
+
+variable "location" {
+    description = "The Azure Region in which all resources should be created."
+    type = string
+    default = "West Europe"
+}
+
+variable "image_id" {
+    description = "The identifier for vm source image built with Packer."
+    type = object({
+        managed_image_name = string
+        managed_image_resource_group_name = string
+    })
+    default = {
+        managed_image_name = "projectPackerImage"
+        managed_image_resource_group_name = "udacity-demo-rg"
+    }
+}
+
+variable "username" {
+  type = string
+  default = "udacity-lbudo"
+}
+
+variable "password" {
+  type = string
+  default = "$m0L_pp_P0w3r"
+}
+
+variable "num_of_instances" {
+    type = number
+    default = 2
+
+    validation {
+    condition     = var.num_of_instances > 0 && var.num_of_instances < 6
+    error_message = "The number of instances must not be more than 5."
+  }
+}
+
+variable "tagging" {
+  type        = map
+  default     = {
+    "work" = "project"
+    "owner" = "Budak"
+  }
+  description = "Tags to be used throughout the project"
+}
